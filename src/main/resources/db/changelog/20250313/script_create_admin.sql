0780b1add3b136d73d8afe787c17a0015f7b7212
Namdar1Ibrakhim <namdaribrahim04@gmail.com>
2025-03-14 01:15:19 +0500

    debug

--- src/main/resources/db/changelog/20250313/script_create_admin.sql
+++ src/main/resources/db/changelog/20250313/script_create_admin.sql
@@ -6,7 +6,7 @@ VALUES ('admin', '$2a$12$fKzGMyLHEU0dAouwckFEmu4RtTy5KfyZfBdBWsF7dd4RDIJQt921a',
 INSERT INTO user_ (username, password, role, created_date, updated_date)
 VALUES ('user', '$2a$12$vMfi0XAulKWKFp4sHURewOePYE1xshcXnckvfOsVk/kzpkqEJW5iG', 'USER', NOW(), NOW());
 
--- Добавление студента
+-- Добавление студента для userId = 2
 INSERT INTO student (first_name, last_name, email, user_id, created_date, updated_date)
 VALUES ('TestStudent', 'TestStudent', 'namdar_ibrahim@mail.ru', 2, NOW(), NOW());
 
